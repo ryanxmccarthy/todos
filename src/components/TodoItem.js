eb7be492ec62fd36731b0a82a3cb2d9357fd039d
Ryan XVX <ryanxmccarthy@gmail.com>
2021-11-26 21:15:44 -0800

    adding todo item file

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class TodoItem extends React.Component {
+    render() {
+        return (
+            <h1>Todo Item</h1>
+        )       
+    }
+}
+
+export default TodoItem
