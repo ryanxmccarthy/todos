4b5337b1ae08a0d1ac3a5bd1b1f15e0c26eba2a3
Ryan XVX <ryanxmccarthy@gmail.com>
2021-11-27 21:04:38 -0800

    setting up header

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function TodoItem(props) {
-    return (
-        <li>{props.todo.title}</li>
-    )       
+class TodoItem extends React.Component {
+    render() {
+        return (
+            <li>{this.props.todo.title}</li>
+        )    
+    }     
 }
 
 export default TodoItem
