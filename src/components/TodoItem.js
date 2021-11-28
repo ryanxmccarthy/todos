009e3bbc1ad87f12b15382383e33e7f1695a12e9
Ryan XVX <ryanxmccarthy@gmail.com>
2021-11-27 21:01:19 -0800

    changing todo item to a function component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-class TodoItem extends React.Component {
-    render() {
-        return (
-            <li>{this.props.todo.title}</li>
-        )       
-    }
+function TodoItem(props) {
+    return (
+        <li>{props.todo.title}</li>
+    )       
 }
 
 export default TodoItem
