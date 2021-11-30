391b916d3430937dcfc5ce0968796bb0a4723e72
Ryan XVX <ryanxmccarthy@gmail.com>
2021-11-29 19:50:46 -0800

    passing handleClick function up

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -31,7 +31,10 @@ class TodoContainer extends React.Component {
         return (
             <div>
                 <Header />
-                <TodosList todos={this.state.todos} />
+                <TodosList 
+                    todos={this.state.todos} 
+                    handleChangeProps={this.handleChange}
+                />
             </div>
         )
     }
