d9de0478fe3a0067697b145c148fdc2971b95cbf
Ryan XVX <ryanxmccarthy@gmail.com>
2021-11-29 19:43:54 -0800

    implementing click handling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,10 @@ class TodoItem extends React.Component {
     render() {
         return (
             <li>
-                <input type='checkbox' />
+                <input 
+                    type='checkbox' 
+                    checked={this.props.todo.completed}
+                />
                 {this.props.todo.title}
             </li>
         )    
