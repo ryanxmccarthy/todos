43c48e1b5e6f3fa2d5250346f303bf01959a3732
Ryan XVX <ryanxmccarthy@gmail.com>
2021-12-01 20:47:49 -0800

    adding delete function

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -36,6 +36,10 @@ class TodoContainer extends React.Component {
             })
         }));
     };
+
+    delTodo = id => {
+        console.log('deleted', id);
+    };
     
     render() {
         return (
@@ -44,6 +48,7 @@ class TodoContainer extends React.Component {
                 <TodosList 
                     todos={this.state.todos} 
                     handleChangeProps={this.handleChange}
+                    deleteTodoProps={this.delTodo}
                 />
             </div>
         )
