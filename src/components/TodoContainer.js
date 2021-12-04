af0d203c94f1798cb92578fe64cad263ed68d55c
Ryan XVX <ryanxmccarthy@gmail.com>
2021-12-04 13:38:12 -0800

    new todos working

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -49,7 +49,15 @@ class TodoContainer extends React.Component {
     };
 
     addTodoItem = title => {
-        console.log(title);
+        const newTodo = {
+            id: 4,
+            title: title,
+            completed: false
+        };
+
+        this.setState({
+            todos:[...this.state.todos, newTodo]
+        });
     };
     
     render() {
