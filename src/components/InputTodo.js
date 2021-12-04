345918c664724015e3b018c1f5a6068cf9a383cc
Ryan XVX <ryanxmccarthy@gmail.com>
2021-12-04 13:24:21 -0800

    adding form submission functionality

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -11,15 +11,20 @@ class InputTodo extends Component {
         });
     };
 
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state.title);
+    };
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <input 
                     name='title'
+                    onChange={this.onChange}
                     placeholder='Add Todo' 
                     type='text' 
-                    value={this.state.title}               
-                    onChange={this.onChange}
+                    value={this.state.title}                                   
                 />
                 <button>Submit</button>
             </form>
