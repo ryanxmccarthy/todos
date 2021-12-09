9ac898199b3bf0115c79c40c913352d410ebfaf8
Ryan XVX <ryanxmccarthy@gmail.com>
2021-12-08 16:02:30 -0800

    styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,19 @@ import React from 'react'
 const Header = () => {
     return (
         <header>
-            <h1>todos</h1>
+            <h1
+                style={{
+                    color: '#444',
+                    fontSize: '6rem',
+                    fontWeight: '600',
+                    lineHeight: '1em',
+                    marginBottom: '2rem',
+                    textAlign: 'center',
+                    textTransform: 'lowercase'
+                }}
+            >
+                todos
+            </h1>
         </header>
     )
 }
