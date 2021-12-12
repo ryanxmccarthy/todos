4388e7b2de1a2b77fde9f30c9cef5f98aab48c1f
Ryan XVX <ryanxmccarthy@gmail.com>
2021-12-11 16:17:07 -0800

    editing working

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -63,7 +63,14 @@ class TodoContainer extends React.Component {
     };
 
     setUpdate = (updatedTitle, id) => {
-        console.log(updatedTitle, id)
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.title = updatedTitle
+                }
+                return todo
+            }),
+        })
     }
     
     render() {
