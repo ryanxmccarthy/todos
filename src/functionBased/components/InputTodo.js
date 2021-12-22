f62e68c40955a10c058701277a06b2cd08b51ac0
Ryan XVX <ryanxmccarthy@gmail.com>
2021-12-22 10:27:32 -0800

    finished updating inputTodo

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -18,7 +18,17 @@ const InputTodo = props => {
     }
 
     return (
-        <form 
+        <form onSubmit={handleSubmit} className='form-container'>
+            <input
+                type='text'
+                className='input-text'
+                placeholder='Add todo'
+                value={title}
+                name='title'
+                onChange={onChange}
+            />
+            <button className='input-submit'>Submit</button>
+        </form>
     )
 }
 
