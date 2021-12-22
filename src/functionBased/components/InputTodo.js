00d5b4b5462648c4328f7336bd6c599ad5d7f991
Ryan XVX <ryanxmccarthy@gmail.com>
2021-12-22 10:25:11 -0800

    still updating components

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react'
 
-const InputTodo = () => {
-    console.log(useState('hello'))
-    return <div></div>
+const InputTodo = props => {
+    const [title, setTitle] = useState('')
+    
+    const onChange = e => {
+        setTitle(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (title.trim()) {
+            props.addTodoProps(title)
+            setTitle('')
+        } else {
+            alert('Please write item')
+        }
+    }
+
+    return (
+        <form 
+    )
 }
 
 export default InputTodo
