af251bb33277d62944d66394ef673230da5e3b07
Ryan XVX <ryanxmccarthy@gmail.com>
2022-01-08 11:33:55 -0800

    still trying to get routes working

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Link, Route, useMatch } from 'react-router-dom'
 
 const About = () => {
+    console.log(useMatch())
     return <h3>Hello from the about page</h3>
 }
 
